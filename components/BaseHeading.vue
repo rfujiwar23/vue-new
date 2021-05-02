b17553771396212e2bfcbd7b029a433f6e755496
Ryo Fujiwara <jjcproject@icloud.com>
2021-05-02 18:33:29 +0900

    up

---
+++ components/BaseHeading.vue
@@ -0,0 +1,21 @@
+<template>
+  <h2>Hello, {{ name }}</h2>
+</template>
+
+<script>
+export default {
+  props: {
+    name: {
+      type: String,
+      default: 'Stranger',
+    },
+  },
+}
+</script>
+
+<style type="text/css">
+h2[data-v-57163366] {
+  font-size: 30px;
+  font-weight: bold;
+}
+</style>
